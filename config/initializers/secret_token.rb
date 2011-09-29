646591887ff806b29fd09b863180d846f34ac45f
David Pombar <david.pombar@gmail.com>
2011-09-29 18:42:10 +0200

    Init commit, scaffold and Postgresql config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twooksqr::Application.config.secret_token = 'a731698482baf3e8a813aa7d5909f152fd0c14ad9dcfef0abf8e2a708842d43d2f408fa95344d8d51051a746818a22d5108e7a7178c1b020ebc4ece32e67d067'
